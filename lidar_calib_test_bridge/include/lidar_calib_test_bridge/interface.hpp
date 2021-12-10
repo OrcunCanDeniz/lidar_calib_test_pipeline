c4ac453806ba68d54cd6aeff1e07000bab3ccb83
OrcunCanDeniz <orcun@adastec.com>
2021-12-10 16:08:09 +0300

    templated eval request func

--- lidar_calib_test_bridge/include/lidar_calib_test_bridge/interface.hpp
+++ lidar_calib_test_bridge/include/lidar_calib_test_bridge/interface.hpp
@@ -31,7 +31,8 @@ class lidar_calib_test_bridge
         lidar_calib_test_bridge();
         void fromTestMsg(const test_comm::test_pointcloud::ConstPtr parent_test_pc, const test_comm::test_pointcloud::ConstPtr child_test_pc);
         lidar_calib_test_comms::calib_result toTestReq();
-        void toEvalSrv(Eigen::Matrix4f guess);
+        template<typename Type>
+        void toEvalSrv(const Eigen::Matrix<Type, 4, 4>& guess);
         void toEvalSrv(geometry_msgs::TransformStamped tf);
         std::string getAgent();
         std::string getScene();
@@ -61,4 +62,34 @@ lidar_calib_test_bridge::lidar_calib_test_bridge(ros::NodeHandle *nh_, ros::Node
     child_sub = new message_filters::Subscriber<lidar_calib_test_comms::test_pointcloud>(*nh_, "child/pointcloud", 1);
     pc_sync_ = new message_filters::Synchronizer<SyncPolicyT>(SyncPolicyT(10), *parent_sub, *child_sub);
     pc_sync_->registerCallback(boost::bind(&lidar_calib_test_bridge::fromTestMsg, this, _1, _2));
+}
+
+template<typename Type>
+void lidar_calib_test_bridge::toEvalSrv(const Eigen::Matrix<Type, 4, 4>& guess)
+{
+    Eigen::Matrix<Type, 3, 3> rot_mat = guess.block(0,0,3,3);
+    Eigen::Quaternion<Type> q(rot_mat);
+    Eigen::Matrix<Type, 3, 1> translation_vector = guess.block(0,3,3,1);
+
+    geometry_msgs::TransformStamped tf;
+    tf.child_frame_id = child_frame;
+    tf.header.frame_id= parent_frame;
+
+    tf.transform.translation.x = translation_vector(0);
+    tf.transform.translation.y = translation_vector(1);
+    tf.transform.translation.z = translation_vector(2);
+
+    tf.transform.rotation.w = q.w();
+    tf.transform.rotation.x = q.x();
+    tf.transform.rotation.y = q.y();
+    tf.transform.rotation.z = q.z();
+
+    test_comm::calib_result srv;
+    srv.request.agent = agent_id;
+    srv.request.scene = scene_id;
+
+    tf.header.stamp = ros::Time::now();    
+    srv.request.transform = tf;
+
+    error_service_client.call(srv);
 }
--- lidar_calib_test_bridge/src/interface.cpp
+++ lidar_calib_test_bridge/src/interface.cpp
@@ -28,34 +28,6 @@ void lidar_calib_test_bridge::fromTestMsg(const test_comm::test_pointcloud::Cons
     native_calibrator_func(parent_pc_ptr, child_pc_ptr);
 }
 
-void lidar_calib_test_bridge::toEvalSrv(Eigen::Matrix4f guess)
-{
-    Eigen::Matrix3f rot_mat = guess.block(0,0,3,3);
-    Eigen::Quaternionf q(rot_mat);
-    Eigen::Vector3f translation_vector = guess.block(0,3,3,1);
-
-    geometry_msgs::TransformStamped tf;
-    tf.child_frame_id = child_frame;
-    tf.header.frame_id= parent_frame;
-
-    tf.transform.translation.x = translation_vector(0);
-    tf.transform.translation.y = translation_vector(1);
-    tf.transform.translation.z = translation_vector(2);
-
-    tf.transform.rotation.w = q.w();
-    tf.transform.rotation.x = q.x();
-    tf.transform.rotation.y = q.y();
-    tf.transform.rotation.z = q.z();
-
-    test_comm::calib_result srv;
-    srv.request.agent = agent_id;
-    srv.request.scene = scene_id;
-
-    tf.header.stamp = ros::Time::now();    
-    srv.request.transform = tf;
-
-    error_service_client.call(srv);
-}
 
 void lidar_calib_test_bridge::toEvalSrv(geometry_msgs::TransformStamped tf)
 {
